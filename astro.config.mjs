2ca63de1d3e0118f9024d39562f6247be75f96f6
Arsen Melikyan <bugron@mail.ru>
2024-01-06 22:53:39 +0400

    Add Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import starlightBlog from 'starlight-blog'
 import starlight from '@astrojs/starlight';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
 	site: 'https://melikyan.dev',
@@ -26,6 +27,32 @@ export default defineConfig({
 				linkedin: 'https://www.linkedin.com/in/arsen-melikyan/',
 				'x.com': 'https://twitter.com/bugron1',
 				youtube: 'https://www.youtube.com/@bugron',
+			},
+			head: [
+				{
+					tag: 'script',
+					attrs: {
+						src: 'https://www.googletagmanager.com/gtag/js?id=G-EWP344X6RY',
+						type: "text/partytown",
+						async: true,
+					},
+				},
+				{
+					tag: 'script',
+					type: "text/partytown",
+					content: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-EWP344X6RY');
+					`
+				},
+			],
+		}),
+		partytown({
+			config: {
+			  forward: ["dataLayer.push"],
 			},
 		}),
 	],
