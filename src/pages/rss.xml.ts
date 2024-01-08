7ca92f1b8c170c9e674ca7177b5b75426cb0dd56
Arsen Melikyan <bugron@mail.ru>
2024-01-08 19:07:31 +0400

    Fix broken RSS links and add a stylesheet

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -16,9 +16,10 @@ export const GET: APIRoute = async (context) => {
     items: posts.map((post) => ({
       ...post.data,
       pubDate: post.data.date ?? new Date('2024-01-07'),
-      link: `/blog/${post.slug}/`,
+      link: post.slug === 'index' ? '/' : `/${post.slug}/`,
       content: sanitizeHtml(parser.render(post.body)),
       ...post.data,
     })),
+    stylesheet: '/rss/pretty-feed-v3.xsl',
   });
 }
