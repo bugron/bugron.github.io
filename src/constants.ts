863827d811495393337bc86f28a9925547db0dc6
Arsen Melikyan <bugron@mail.ru>
2024-09-01 17:00:22 +0400

    Auto-format the project

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
-export const BLOG_TITLE = 'Arsen Melikyan\'s personal blog';
-export const BLOG_DESCRIPTION = 'Welcome to my personal blog where I share my thoughts about web development, cybersecurity, etc.';
-export const BLOG_URL = 'https://melikyan.dev';
+export const BLOG_TITLE = "Arsen Melikyan's personal blog";
+export const BLOG_DESCRIPTION =
+    "Welcome to my personal blog where I share my thoughts about web development, cybersecurity, etc.";
+export const BLOG_URL = "https://melikyan.dev";
